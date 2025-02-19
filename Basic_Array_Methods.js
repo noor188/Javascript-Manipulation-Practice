979b95afeb83588aa94cf5ef5b5ef080f7c3c778
noor188 <noormmlk@gmail.com>
2025-02-19 13:18:39 -0700

    Ticket 1 setup

---
+++ Basic_Array_Methods.js
@@ -0,0 +1,11 @@
+// Ticket 1: Basic Array Methods
+
+// Add a new employee to the end of the array
+let employees = ['John', 'Sarah', 'Eric'];
+
+employees.push("Noor");
+
+console.log(employees);
+
+
+
